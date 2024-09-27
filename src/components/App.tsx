6582b311f428005c6d3bfff617ec243a892c3433
Andrew Huth <git@huth.me>
2024-09-27 07:41:42 -0400

    Add spritesheet and display it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,43 @@
+import {useEffect, useRef} from 'react';
+import spriteSheetUrl from '../catspritesx4.gif';
+
 export default function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+
+    // Sprite properties
+    const spriteWidth = 64;
+    const spriteHeight = 64;
+
+    const spriteImage = new Image();
+    spriteImage.src = spriteSheetUrl;
+
+    spriteImage.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.drawImage(
+        spriteImage,
+        0,
+        0,
+        spriteWidth,
+        spriteHeight,
+        0,
+        0,
+        spriteWidth,
+        spriteHeight,
+      );
+    };
+  }, []);
+
   return (
-    <div className="p-8 text-center">
-      <h1 className="text-2xl">Hello World</h1>
+    <div className="p-8">
+      <canvas
+        className="mx-auto h-full max-h-[300px] w-full max-w-[400px]"
+        ref={canvasRef}
+      />
     </div>
   );
 }
