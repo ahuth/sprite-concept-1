87fb72a771ba0baf531df36413fda04a08f08b69
Andrew Huth <git@huth.me>
2024-09-27 10:45:21 -0400

    Don't reset x position when changing direction

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,9 @@ export default function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [direction, setDirection] = useState(0); // 0: no movement, -1: left, 1: right
 
+  // Store the x position in a ref so we don't lose it when setting up and tearing down effects.
+  const xRef = useRef(0);
+
   // Key handlers for direction.
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
@@ -40,13 +43,10 @@ export default function App() {
     const spriteHeight = 64;
     const numFrames = 6;
     const animationFramesBetweenSpriteFrame = 10;
-    let frameX = 0;
-    const frameY = 1;
-    let gameFrame = 0;
-
-    // Sprite position
-    let x = 0;
     const y = canvas.height - spriteHeight;
+    const frameY = 1;
+    let frameX = 0;
+    let gameFrame = 0;
 
     // Load sprite image
     const spriteImage = new Image();
@@ -76,21 +76,23 @@ export default function App() {
         frameY * spriteHeight,
         spriteWidth,
         spriteHeight,
-        direction === 1 ? -x - spriteWidth : x,
+        direction === 1 ? -xRef.current - spriteWidth : xRef.current,
         y,
         spriteWidth,
         spriteHeight,
       );
 
-      // Restore the context to its original state before we flipped it.
-      ctx.restore();
+      if (direction === 1) {
+        // Restore the context to its original state before we flipped it.
+        ctx.restore();
+      }
 
       // Move sprite based on direction
-      x += direction * 2;
-      if (x > canvas.width) {
-        x = -spriteWidth;
-      } else if (x < -spriteWidth) {
-        x = canvas.width;
+      xRef.current += direction * 2;
+      if (xRef.current > canvas.width) {
+        xRef.current = -spriteWidth;
+      } else if (xRef.current < -spriteWidth) {
+        xRef.current = canvas.width;
       }
 
       gameFrame++;
